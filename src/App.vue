505a5911241ec1697839ca6923eca661ad5d3816
nageshkumawat <nageshwar@aarrsolprivatelimited.com>
2022-07-11 15:21:45 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <router-view/>
+  <RouterLink to="/home" >Home | </RouterLink>
+  <RouterLink to="/" > Index</RouterLink>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
